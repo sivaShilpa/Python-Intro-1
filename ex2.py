125cbcd32e2e1f52f4653ee1c65d323ca2d9670d
sivaShilpa <shilpa.vchand@gmail.com>
2022-08-05 14:38:01 -0400

    2/13

---
+++ ex2.py
@@ -0,0 +1,6 @@
+def array_to_string(ints):
+    output = ''
+    for i in ints:
+        output += str(i) + ' '
+
+    return output
--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 from ex1 import hello_world
+from ex2 import array_to_string
 
-hello_world("3")
+# ex1
+# hello_world("3")
+
+# ex2
+array = [1, 2, 3]
+result = array_to_string(array)
+print(result)
